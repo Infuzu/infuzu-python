53ccdee37cdf137f14465ac5e2b03e20511ca1cd
Yidi Sprei <yidi.sprei@yidienterprises.com>
2025-02-24 16:33:23 -0500

    feat: add version retrieval utility and refactor main execution
    - Introduced `get_version` function in `utils.py` to read and validate
      the application's version from a `.package-version` file.
    - Updated `__init__.py` to include `get_version` in the module
      exports, allowing for easy access to version information.
    - Encapsulated the logic in `main.py` under a `__main__` check
      to ensure the environment is only loaded when the script is
      executed directly, promoting better modularity and testability.

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
-from src.infuzu import (create_chat_completion, ChatCompletionsHandlerRequestMessage, ChatCompletionsObject)
+from src.infuzu import (
+    create_chat_completion, ChatCompletionsHandlerRequestMessage, ChatCompletionsObject
+)
 from dotenv import load_dotenv
 
 
-load_dotenv()
+if __name__ == "__main__":
+    load_dotenv()
 
+    messages: list[ChatCompletionsHandlerRequestMessage] = [
+        ChatCompletionsHandlerRequestMessage(role="system", content="You are a helpful assistant."),
+        ChatCompletionsHandlerRequestMessage(role="user", content="What is the capital of France?"),
+    ]
 
-messages: list[ChatCompletionsHandlerRequestMessage] = [
-    ChatCompletionsHandlerRequestMessage(role="system", content="You are a helpful assistant."),
-    ChatCompletionsHandlerRequestMessage(role="user", content="What is the capital of France?"),
-]
-
-
-try:
-    response: ChatCompletionsObject = create_chat_completion(messages=messages)
-    print(response)
-except Exception as e:
-    print(f"Error: {e}")
+    try:
+        response: ChatCompletionsObject = create_chat_completion(messages=messages)
+        print(response)
+    except Exception as e:
+        print(f"Error: {e}")

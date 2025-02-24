0755a2886adc88fc1220898533ecd2fbab7ec7bf
Yidi Sprei <yidi.sprei@yidienterprises.com>
2025-02-24 15:27:36 -0500

    feat: enhance chat completion with detailed response objects
    This commit introduces multiple classes representing different parts
    of the chat completion response, such as ChatCompletionsChoiceObject,
    ChatCompletionsChoiceMessageObject, and more. These changes improve
    the codebase by providing a structured and detailed representation of
    the API response, enhancing readability and maintainability. The
    return type of the `create_chat_completion` function is now
    `ChatCompletionsObject`, ensuring type safety and clarity. Additionally,

--- src/infuzu/__init__.py
+++ src/infuzu/__init__.py
@@ -1,12 +1,41 @@
 from .api_client import (
-    create_chat_completion, ChatCompletionsHandlerRequestMessage, ChatCompletionsRequestContentPart
+    create_chat_completion,
+    ChatCompletionsHandlerRequestMessage,
+    ChatCompletionsRequestContentPart,
+    ChatCompletionsChoiceMessageAudioObject,
+    ChatCompletionsChoiceMessageFunctionCallObject,
+    ChatCompletionsChoiceMessageToolCallFunctionObject,
+    chatCompletionsChoiceMessageToolCallObject,
+    ChatCompletionsChoiceMessageObject,
+    ChatCompletionsChoiceLogprobsItemTopLogprobObject,
+    ChatCompletionsLogprobsItemObject,
+    ChatCompletionsChoiceLogprobsObject,
+    ChatCompletionsChoiceModelObject,
+    ChatCompletionsChoiceErrorObject,
+    ChatCompletionsChoiceLatencyObject,
+    ChatCompletionsChoiceObject,
+    ChatCompletionsObject,
 )
 from .errors import (InfuzuAPIError, APIWarning, APIError)
+
 
 __all__: list[str] = [
     "create_chat_completion",
     "ChatCompletionsRequestContentPart",
     "ChatCompletionsHandlerRequestMessage",
+    "ChatCompletionsChoiceMessageAudioObject",
+    "ChatCompletionsChoiceMessageFunctionCallObject",
+    "ChatCompletionsChoiceMessageToolCallFunctionObject",
+    "chatCompletionsChoiceMessageToolCallObject",
+    "ChatCompletionsChoiceMessageObject",
+    "ChatCompletionsChoiceLogprobsItemTopLogprobObject",
+    "ChatCompletionsLogprobsItemObject",
+    "ChatCompletionsChoiceLogprobsObject",
+    "ChatCompletionsChoiceModelObject",
+    "ChatCompletionsChoiceErrorObject",
+    "ChatCompletionsChoiceLatencyObject",
+    "ChatCompletionsChoiceObject",
+    "ChatCompletionsObject",
 
     "InfuzuAPIError",
     "APIWarning",
